0d444fa7e532b0953e5457775e1bc6407029f4ce
Simon Emmanuel <simonemmanuel743@gmail.com>
2024-02-15 13:33:26 +0100

    makes the app component clean

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,10 @@ import CityList from "./components/CityList";
 import City from "./components/City";
 import CountryList from "./components/CountryList";
 import Form from "./components/Form";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
 
 function App() {
   return (
